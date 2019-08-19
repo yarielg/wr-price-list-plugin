8c8a97653965c9320070ad899f10c617d86aeb11
Yariel Gordillo <yariel.gordillo@gmail.com>
2019-08-18 20:04:23 -0400

    Fixing category filter

--- uninstall.php
+++ uninstall.php
@@ -13,7 +13,7 @@ global $wpdb;
 $table_name1 = $wpdb->prefix . 'wr_price_lists';
 $table_name2 = $wpdb->prefix . 'wr_price_lists_price';
 $table_name3 = $wpdb->prefix . 'wr_rules';
-$wpdb->query('DELETE FROM ' .$wpdb->prefix . 'options WHERE option_name LIKE "wrpl%"');
+$wpdb->query('DELETE FROM ' .$wpdb->prefix . 'options WHERE option_name LIKE "wrpl%" AND  option_name NOT LIKE "wrpl_role%"');
 $wpdb->query( "DROP TABLE IF EXISTS $table_name1" );
 $wpdb->query( "DROP TABLE IF EXISTS $table_name2" );
 $wpdb->query( "DROP TABLE IF EXISTS $table_name3" );
